8132c8146531a0411e054df7ce9a3b7ddf53a62c
Andre Friese <andrefriese@t-online.de>
2014-10-07 20:42:39 +0200

    Added toogle between view and edit

--- fileinfo.js
+++ fileinfo.js
@@ -1,4 +1,57 @@
 $(function(){
+ko.bindingHandlers.radio = {
+  init: function(element, valueAccessor, allBindings, data, context) {
+    var $buttons, $element, elementBindings, observable;
+    observable = valueAccessor();
+    if (!ko.isWriteableObservable(observable)) {
+      throw "You must pass an observable or writeable computed";
+    }
+    $element = $(element);
+    if ($element.hasClass("btn")) {
+      $buttons = $element;
+    } else {
+      $buttons = $(".btn", $element);
+    }
+    elementBindings = allBindings();
+    $buttons.each(function() {
+      var $btn, btn, radioValue;
+      btn = this;
+      $btn = $(btn);
+      radioValue = elementBindings.radioValue || $btn.attr("data-value") || $btn.attr("value") || $btn.text();
+      $btn.on("click", function() {
+        observable(ko.utils.unwrapObservable(radioValue));
+      });
+      return ko.computed({
+        disposeWhenNodeIsRemoved: btn,
+        read: function() {
+          $btn.toggleClass("active", observable() === ko.utils.unwrapObservable(radioValue));
+        }
+      });
+    });
+  }
+};
+
+ko.bindingHandlers.checkbox = {
+  init: function(element, valueAccessor, allBindings, data, context) {
+    var $element, observable;
+    observable = valueAccessor();
+    if (!ko.isWriteableObservable(observable)) {
+      throw "You must pass an observable or writeable computed";
+    }
+    $element = $(element);
+    $element.on("click", function() {
+      observable(!observable());
+    });
+    ko.computed({
+      disposeWhenNodeIsRemoved: element,
+      read: function() {
+        $element.toggleClass("active", observable());
+      }
+    });
+  }
+};
+
+
 	ko.applyBindings(FileModel(model));
 	console.log("JHJH")
 });
@@ -15,6 +68,7 @@ var model =	{
 
 
 var FileModel = function(data){
+	this.editmode = ko.observable("false");
 	this.fields = ko.observableArray(data.fields);
 }
 
@@ -23,3 +77,5 @@ var Field = function(data){
 	self.name = ko.observable(data.name);
 	self.data = ko.observable(data.data);
 }
+
+
